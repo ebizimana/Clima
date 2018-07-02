5f4d80d74cec6603bc2bdf85ac6b6854946dfc6a
ebizimana <ebizimana@liberty.edu>
2018-07-02 11:13:40 -0400

    Finished Setting up the Location Manager to get the GPS Data from the iPhone

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -35,7 +35,8 @@ class WeatherViewController: UIViewController,CLLocationManagerDelegate {
         
         //TODO:Set up the location manager here.
         locationManager.delegate = self
-        
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
         
     }
